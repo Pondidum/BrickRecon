54751d59cb3ea25dbd3b053bc9234a253648de47
andy <andy@andydote.co.uk>
2017-07-15 15:25:06 +0300

    webui: basic setup

---
+++ infra/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  default = "eu-west-1" # Irish region best region!
+}
+
+provider "aws" {
+  profile = "default"
+  region = "${var.region}"
+}
+
+// this will fetch our account_id, no need to hard code it
+data "aws_caller_identity" "current" {
+}
+
+variable "environment" {
+  default = "dev"
+}
---
+++ infra/webui.tf
@@ -0,0 +1,33 @@
+variable "bucket" {
+  default = "brickrecon"
+}
+
+data "template_file" "s3_public_policy" {
+  template = "${file("policies/s3-static-site.json")}"
+  vars {
+    bucket_name = "${var.bucket}_${var.environment}"
+  }
+}
+
+resource "aws_s3_bucket" "webui" {
+  bucket = "${var.bucket}_${var.environment}"
+
+  acl = "public-read" # lock this down to just the website later
+  policy = "${data.template_file.s3_public_policy.rendered}"
+
+  website {
+    index_document = "index.html"
+  }
+
+  tags = {
+    environment = "${var.environment}"
+  }
+}
+
+resource "aws_s3_bucket_object" "index" {
+  bucket = "${aws_s3_bucket.webui.bucket}"
+  key = "index.html"
+  source = "../src/webui/index.html"
+  content_type = "text/html"
+  etag = "${md5(file("../src/webui/index.html"))}"
+}
