feef1512cc3de8acda43528d10ac2243cf4f2355
andy <andy@andydote.co.uk>
2017-07-23 14:44:27 +0300

    webui: add part images to the part list

--- src/webui/src/partList/index.js
+++ src/webui/src/partList/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { Table, Thead, Th } from 'reactable'
 
+const imageSize = {
+  width: 66,
+  height: 50
+}
 const columns = [
+  { key: 'image', label: 'Image', ...imageSize },
   { key: 'partNumber', label: 'Part No' },
   { key: 'name', label: 'Name' },
   { key: 'color', label: 'Color' },
@@ -10,17 +15,32 @@ const columns = [
 ]
 
 const header = (col, i) =>
-  <Th key={i} column={col.key}>
+  <Th key={i} column={col.key} width={col.width ? col.width : null}>
     <strong className="name-header">
       {col.label}
     </strong>
   </Th>
 
-const PartList = ({ parts }) =>
-  <Table className="table table-hover" sortable={true} data={parts}>
-    <Thead>
-      {columns.map(header)}
-    </Thead>
-  </Table>
+const PartList = ({ parts }) => {
+  const withImages = parts.map(part =>
+    Object.assign({}, part, {
+      image: (
+        <img
+          src={`/images/parts/${part.partNumber}-${part.color}.png`}
+          alt={`part ${part.partNumber}`}
+          width={imageSize.width + 'px'}
+          height={imageSize.height + 'px'}
+        />
+      )
+    })
+  )
+  return (
+    <Table className="table table-hover" sortable={true} data={withImages}>
+      <Thead>
+        {columns.map(header)}
+      </Thead>
+    </Table>
+  )
+}
 
 export default PartList
