bc674fa9fe3aded237e999c79f3df9cac69d6f0a
andy <andy@andydote.co.uk>
2017-07-23 14:23:42 +0300

    webui: style and enable sorting on partList

--- src/webui/src/partList/index.js
+++ src/webui/src/partList/index.js
@@ -1,6 +1,26 @@
 import React from 'react'
-import { Table } from 'reactable'
+import { Table, Thead, Th } from 'reactable'
 
-const PartList = ({ parts }) => <Table className="table" data={parts} />
+const columns = [
+  { key: 'partNumber', label: 'Part No' },
+  { key: 'name', label: 'Name' },
+  { key: 'color', label: 'Color' },
+  { key: 'quantity', label: 'Quantity' },
+  { key: 'category', label: 'Category' }
+]
+
+const header = (col, i) =>
+  <Th key={i} column={col.key}>
+    <strong className="name-header">
+      {col.label}
+    </strong>
+  </Th>
+
+const PartList = ({ parts }) =>
+  <Table className="table table-hover" sortable={true} data={parts}>
+    <Thead>
+      {columns.map(header)}
+    </Thead>
+  </Table>
 
 export default PartList
