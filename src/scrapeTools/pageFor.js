2fee930e6bd3e00a05e0f551999044b2ceff5cbf
andy <andy@andydote.co.uk>
2017-07-26 23:55:27 +0300

    scrapeTools: fetch the search page

--- src/scrapeTools/pageFor.js
+++ src/scrapeTools/pageFor.js
@@ -1,5 +1,13 @@
-const forPart = partNumber => {}
+const fetch = require('node-fetch')
 
-module.export = {
+const forPart = partNumber => {
+  const searchUrl = `http://www.brickowl.com/search/catalog?query=${partNumber}&cat=1`
+
+  return fetch(searchUrl)
+    .then(res => res.text())
+    .then(body => console.log(body))
+}
+
+module.exports = {
   part: forPart
 }
