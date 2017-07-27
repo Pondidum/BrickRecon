5e0c221b6e42fda7fd07f4ea554ded12a974d5c3
andy <andy@andydote.co.uk>
2017-07-27 22:02:25 +0300

    scrapeTools: implment basic page discovery from search

--- src/scrapeTools/pageFor.js
+++ src/scrapeTools/pageFor.js
@@ -1,11 +1,17 @@
 const fetch = require('node-fetch')
+const cheerio = require('cheerio')
+
+const root = 'http://www.brickowl.com'
 
 const forPart = partNumber => {
-  const searchUrl = `http://www.brickowl.com/search/catalog?query=${partNumber}&cat=1`
+  const searchUrl = `${root}/search/catalog?query=${partNumber}&cat=1`
 
-  return fetch(searchUrl)
-    .then(res => res.text())
-    .then(body => console.log(body))
+  return fetch(searchUrl).then(res => res.text()).then(body => {
+    const doc = cheerio.load(body)
+    const fragment = doc('ul.category-grid li a').attr('href')
+
+    return root + fragment
+  })
 }
 
 module.exports = {
--- src/scrapeTools/pageFor.test.js
+++ src/scrapeTools/pageFor.test.js
@@ -2,8 +2,12 @@ const pageFor = require('./pageFor')
 
 describe('pageFor.part', () => {
   it('should return the url for a known part', () => {
-    expect(pageFor.part(30374)).toEqual(
-      'http://www.brickowl.com/catalog/lego-bar-4l-1-x-4-21462-30374'
-    )
+    pageFor
+      .part(30374)
+      .then(url =>
+        expect(url).toEqual(
+          'http://www.brickowl.com/catalog/lego-bar-4l-1-x-4-21462-30374'
+        )
+      )
   })
 })
