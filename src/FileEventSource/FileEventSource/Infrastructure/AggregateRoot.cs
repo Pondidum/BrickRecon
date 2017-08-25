405d504dab50d9817f923c409f4fdcd611e58705
andy <andy@andydote.co.uk>
2017-08-25 21:56:50 +0300

    fileEventSource: lift aggregateRoot from crispin project

---
+++ src/FileEventSource/FileEventSource/Infrastructure/AggregateRoot.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FileEventSource.Infrastructure
+{
+	public abstract class AggregateRoot : IEvented
+	{
+		private readonly Dictionary<Type, Action<object>> _handlers;
+		private readonly List<object> _pendingEvents;
+
+		protected AggregateRoot()
+		{
+			_handlers = new Dictionary<Type, Action<object>>();
+			_pendingEvents = new List<object>();
+		}
+
+		public Guid ID { get; protected set; }
+
+		protected void Register<TEvent>(Action<TEvent> handler) 
+			where TEvent : Event
+		{
+			_handlers.Add(typeof(TEvent), e => handler((TEvent)e));
+		}
+
+		protected void ApplyEvent<TEvent>(TEvent @event)
+			where TEvent : Event
+		{
+			@event.TimeStamp = DateTime.Now;
+
+			_pendingEvents.Add(@event);
+			_handlers[@event.GetType()](@event);
+
+			PopulateExtraEventData(@event);
+		}
+
+		protected virtual void PopulateExtraEventData(Event @event)
+		{
+			@event.AggregateID = ID;
+		}
+
+		IEnumerable<object> IEvented.GetPendingEvents()
+		{
+			return _pendingEvents;
+		}
+
+		void IEvented.ClearPendingEvents()
+		{
+			_pendingEvents.Clear();
+		}
+
+		void IEvented.LoadFromEvents(IEnumerable<object> events)
+		{
+			foreach (var @event in events)
+				_handlers[@event.GetType()](@event);
+		}
+	}
+}
---
+++ src/FileEventSource/FileEventSource/Infrastructure/Event.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FileEventSource.Infrastructure
+{
+	public abstract class Event
+	{
+		public Guid AggregateID { get; set; }
+		public DateTime TimeStamp { get; set; }
+	}
+}
