a50c8581308e809460a7c5dd76fd56e816172b76
andy <andy@andydote.co.uk>
2017-11-11 14:22:53 +0200

    bsxProcessor: move code to src directory

--- src/BsxProcessor/build.sh
+++ src/BsxProcessor/build.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_PATH="$DIR/BsxProcessor/BsxProcessor.csproj"
+PROJECT_PATH="$DIR/src/BsxProcessor/BsxProcessor.csproj"
 
 dotnet restore $PROJECT_PATH
 dotnet publish $PROJECT_PATH
