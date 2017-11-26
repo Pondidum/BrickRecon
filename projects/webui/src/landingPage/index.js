502e210703e273f3e02c00cc2dcb77d5c6fa133a
andy <andy@andydote.co.uk>
2017-11-26 12:52:01 +0200

    webui: remove warning from landingPage

--- projects/webui/src/landingPage/index.js
+++ projects/webui/src/landingPage/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { Col } from 'react-bootstrap'
-import { Route } from 'react-router'
-import Sidebar from '../headerBar'
-import ModelPage from '../modelPage'
+import React from "react";
+import { Route } from "react-router";
+import Sidebar from "../headerBar";
+import ModelPage from "../modelPage";
 
-const LandingPage = ({ match }) =>
+const LandingPage = ({ match }) => (
   <div>
     <Sidebar match={match} />
     <Route path="/model/:name" component={ModelPage} />
   </div>
+);
 
-export default LandingPage
+export default LandingPage;
