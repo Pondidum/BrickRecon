91fbae3c4202bf7599b5a051d7635969612b4e6d
andy <andy@andydote.co.uk>
2017-11-26 12:50:59 +0200

    webui: optimise partList construction

--- projects/webui/src/partList/index.js
+++ projects/webui/src/partList/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Table, Thead, Th } from "reactable";
+import naturalSort from "javascript-natural-sort";
 import PartImage from "./partImage";
 import PartLinks from "./partLinks";
-import naturalSort from "javascript-natural-sort";
 
 const imageSize = {
   width: 66,
   height: 50
 };
-const columns = [
+
+const buildHeader = col => (
+  <Th key={col.key} column={col.key} width={col.width ? col.width : null}>
+    <strong className="name-header">{col.label}</strong>
+  </Th>
+);
+
+const headers = [
   { key: "image", label: "Image", ...imageSize },
   { key: "partNumber", label: "Part No" },
   { key: "name", label: "Name" },
@@ -16,43 +23,36 @@ const columns = [
   { key: "quantity", label: "Quantity" },
   { key: "category", label: "Category" },
   { key: "links", label: "Links" }
+].map(buildHeader);
+
+const sort = [
+  { column: "partNumber", sortFunction: naturalSort },
+  { column: "name", sortFunction: naturalSort },
+  { column: "colorName" },
+  { column: "quantity", sortFunction: naturalSort },
+  { column: "category" }
 ];
 
-const header = (col, i) => (
-  <Th key={i} column={col.key} width={col.width ? col.width : null}>
-    <strong className="name-header">{col.label}</strong>
-  </Th>
-);
-
-const PartList = ({ parts }) => {
-  const withImages = parts.map(part =>
-    Object.assign({}, part, {
-      image: <PartImage part={part} />,
-      links: <PartLinks part={part} />
-    })
-  );
-  const sort = [
-    { column: "partNumber", sortFunction: naturalSort },
-    { column: "name", sortFunction: naturalSort },
-    { column: "colorName" },
-    { column: "quantity", sortFunction: naturalSort },
-    { column: "category" }
-  ];
-
-  const defaultSort = {
-    column: "name",
-    direction: "asc"
-  };
-  return (
-    <Table
-      className="table table-hover"
-      data={withImages}
-      sortable={sort}
-      defaultSort={defaultSort}
-    >
-      <Thead>{columns.map(header)}</Thead>
-    </Table>
-  );
+const defaultSort = {
+  column: "name",
+  direction: "asc"
 };
+
+const enrich = part =>
+  Object.assign({}, part, {
+    image: <PartImage part={part} />,
+    links: <PartLinks part={part} />
+  });
+
+const PartList = ({ parts }) => (
+  <Table
+    className="table table-hover"
+    data={parts.map(enrich)}
+    sortable={sort}
+    defaultSort={defaultSort}
+  >
+    <Thead>{headers}</Thead>
+  </Table>
+);
 
 export default PartList;
