2480ca9e5f705e30d850c71b90aecc419604fccc
andy <andy@andydote.co.uk>
2017-12-13 21:41:19 +0200

    webui: add deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,5 @@ $variables="../variables.dev.json"
 (cd projects/webui && terraform apply --var-file $variables)
 (cd projects/ImageCache && terraform apply --var-file $variables)
 (cd projects/BsxProcessor && terraform apply --var-file $variables)
+
+(cd projects/webui && ./upload.sh)
