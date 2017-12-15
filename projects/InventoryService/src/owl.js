d5db353a7b74e5aa0222941b50c52595a01d0daa
andy <andy@andydote.co.uk>
2017-12-15 13:58:33 +0200

    owl: group parts by alt_link, allow fetch injection

--- projects/InventoryService/src/owl.js
+++ projects/InventoryService/src/owl.js
@@ -3,33 +3,43 @@ const fetch = require("node-fetch");
 const brickowl = `https://api.brickowl.com/v1`;
 const key = process.env.BRICKOWL_TOKEN;
 
+const defaultClient = uri => fetch(uri).then(res => res.json());
+
 class Owl {
   constructor(client) {
-    this.fetch = client || fetch;
+    this.fetch = client || defaultClient;
   }
 
   getBoid(setId) {
     const uri = `${brickowl}/catalog/id_lookup?key=${key}&type=Set&id=${setId}`;
 
-    return fetch(uri)
-      .then(res => res.json())
-      .then(json => (json.boids.length > 0 ? json.boids[0] : undefined));
+    return this.fetch(uri).then(
+      json => (json.boids.length > 0 ? json.boids[0] : undefined)
+    );
   }
 
   getInventory(boid) {
     const uri = `${brickowl}/catalog/inventory?key=${key}&boid=${boid}`;
 
-    return fetch(uri)
-      .then(res => res.json())
-      .then(json => {
-        if (!json.inventory) {
-          return undefined;
-        }
+    return this.fetch(uri).then(json => {
+      if (!json.inventory) {
+        return undefined;
+      }
 
-        return json.inventory.map(item => {
-          return { quantity: Number(item.quantity), boids: [item.boid] };
+      const grouped = json.inventory.reduce((acc, item) => {
+        const id = item.alt_link === 0 ? item.boid : item.alt_link;
+        const group = (acc[id] = acc[id] || {
+          boids: []
         });
-      });
+
+        group.quantity = Number(item.quantity);
+        group.boids.push(item.boid);
+
+        return acc;
+      }, {});
+
+      return Object.values(grouped).sort((x, y) => x.boids[0] > y.boids[0]);
+    });
   }
 }
 
--- projects/InventoryService/src/owl.test.js
+++ projects/InventoryService/src/owl.test.js
@@ -3,28 +3,51 @@ import Owl from "./owl";
 const owl = new Owl();
 
 describe("getBoid", () => {
-  it("should lookup an id", () => {
-    return owl.getBoid(75042).then(id => expect(id).toEqual("529600"));
-  });
+  it("should lookup an id", () =>
+    owl.getBoid(75042).then(id => expect(id).toEqual("529600")));
 
-  it("should return undefined for unrecognised number", () => {
-    return owl.getBoid(23131231).then(id => expect(id).toBeUndefined());
-  });
+  it("should return undefined for unrecognised number", () =>
+    owl.getBoid(23131231).then(id => expect(id).toBeUndefined()));
 });
 
 describe("getInventory", () => {
-  it("should fetch a set", () => {
-    return owl.getInventory(98236).then(inventory => {
-      expect(inventory).toEqual([
-        { quantity: 1, boids: ["198888"] },
-        { quantity: 1, boids: ["771344-81"] }
-      ]);
-    });
-  });
+  it("should fetch a set", () =>
+    owl
+      .getInventory(98236)
+      .then(inventory =>
+        expect(inventory).toEqual([
+          { quantity: 1, boids: ["198888"] },
+          { quantity: 1, boids: ["771344-81"] }
+        ])
+      ));
 
-  it("should handle bad set ids", () => {
-    return owl.getInventory(12313131323).then(result => {
-      expect(result).toBeUndefined();
-    });
+  it("should handle bad set ids", () =>
+    owl
+      .getInventory(12313131323)
+      .then(result => expect(result).toBeUndefined()));
+
+  it("should group alternalte parts", () => {
+    const testFetch = () =>
+      Promise.resolve({
+        inventory: [
+          { boid: "nogroup-1", quantity: "1", alt_link: 0 },
+          { boid: "group1-1", quantity: "1", alt_link: 1 },
+          { boid: "group2-1", quantity: "1", alt_link: 2 },
+          { boid: "group2-2", quantity: "1", alt_link: 2 },
+          { boid: "nogroup-2", quantity: "1", alt_link: 0 },
+          { boid: "group1-2", quantity: "1", alt_link: 1 }
+        ]
+      });
+
+    return new Owl(testFetch)
+      .getInventory(123)
+      .then(inventory =>
+        expect(inventory).toEqual([
+          { quantity: 1, boids: ["group1-1", "group1-2"] },
+          { quantity: 1, boids: ["group2-1", "group2-2"] },
+          { quantity: 1, boids: ["nogroup-1"] },
+          { quantity: 1, boids: ["nogroup-2"] }
+        ])
+      );
   });
 });
