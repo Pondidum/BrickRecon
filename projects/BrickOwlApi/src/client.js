55375b2a0cdc25cdd2f88d348743c4a42a4e2b3f
andy <andydavies2020@gmail.com>
2018-01-14 19:54:05 +0200

    brickowl: implement client api

---
+++ projects/BrickOwlApi/src/client.js
@@ -0,0 +1,104 @@
+import fetch from "node-fetch";
+import queryString from "query-string";
+
+const chunk = (arr, size = 100) => {
+  var results = [];
+
+  while (arr.length) {
+    results.push(arr.splice(0, size));
+  }
+
+  return results;
+};
+
+const buildQuery = (path, token, query) => {
+  const map = Object.assign({ key: token }, query);
+  const qs = queryString.stringify(map);
+
+  return `https://api.brickowl.com/v1/catalog/${path}?${qs}`;
+};
+
+const combineAlternateParts = inventory => {
+  const grouped = inventory.reduce((acc, item) => {
+    const id = item.alt_link === 0 ? item.boid : item.alt_link;
+    const group = (acc[id] = acc[id] || { boids: [] });
+
+    group.quantity = Number(item.quantity);
+    group.boids.push(item.boid);
+
+    return acc;
+  }, {});
+
+  return Object.keys(grouped).map(key => grouped[key]);
+};
+
+const bestPartNumber = ids => {
+  const map = ids.reduce((all, current) => {
+    all[current.type] = all[current.type] || [];
+    all[current.type].push(current.id);
+    return all;
+  }, {});
+
+  if (map.ldraw) return map.ldraw[0];
+  if (map.peeron_id) return map.peeron_id[0];
+  if (map.design_id) return map.design_id[0];
+};
+
+//---------------------------------------------------------------------------//
+
+const getSetBoid = (fetcher, token, setNumber) => {
+  const uri = buildQuery("id_lookup", token, { type: "Set", id: setNumber });
+  const validBoids = js => js && !js.error && js.boids && js.boids.length > 0;
+
+  return fetcher(uri)
+    .then(json => (validBoids(json) ? json.boids : []))
+    .then(boids => boids[0]);
+};
+
+const getInventory = (fetcher, token, boid) => {
+  const uri = buildQuery("inventory", token, { boid: boid });
+
+  return fetcher(uri)
+    .then(json => json.inventory)
+    .then(inventory => (inventory ? combineAlternateParts(inventory) : []));
+};
+
+const getPartNumbers = (fetcher, batchSize, token, boids) => {
+  const chunks = chunk(
+    boids.map(boid => boid.replace(/(-.*)/g, "")),
+    batchSize
+  );
+
+  const queries = chunks.map(groups => {
+    const uri = buildQuery("bulk_lookup", token, { boids: groups.join(",") });
+    return fetcher(uri)
+      .then(response => response.items)
+      .then(parts =>
+        Object.keys(parts).reduce((all, current) => {
+          all[current] = bestPartNumber(parts[current].ids);
+          return all;
+        }, {})
+      );
+  });
+
+  return Promise.all(queries).then(maps => Object.assign({}, ...maps));
+};
+
+const defaultOptions = {
+  defaultFetcher: uri => fetch(uri).then(res => res.json()),
+  batchSize: 100
+};
+
+class Client {
+  constructor(token, options) {
+    const allOptions = Object.assign({}, defaultOptions, options);
+
+    this.getSetBoid = setNumber =>
+      getSetBoid(options.fetcher, token, setNumber);
+    this.getInventory = boid => getInventory(options.fetcher, token, boid);
+    this.getPartNumbers = boids =>
+      getPartNumbers(options.fetcher, options.batchSize, token, boids);
+  }
+}
+
+export default Client;
---
+++ projects/BrickOwlApi/src/client.test.js
@@ -0,0 +1,143 @@
+import Client from "./client";
+
+let fetcher, client;
+
+beforeEach(() => {
+  fetcher = jest.fn();
+  client = new Client("TOKEN_WAT", {
+    fetcher: fetcher,
+    batchSize: 5
+  });
+});
+
+const mockResponse = res => fetcher.mockReturnValue(Promise.resolve(res));
+
+describe("getSetBoid", () => {
+  it("should read the response", () => {
+    mockResponse({ boids: ["529600"] });
+    return client.getSetBoid(75042).then(id => expect(id).toBe("529600"));
+  });
+
+  it("should an handle unrecognised boid", () => {
+    mockResponse({ boids: [] });
+    return client.getSetBoid(75042).then(id => expect(id).toBe(undefined));
+  });
+});
+
+describe("getInventory", () => {
+  it("should handle bad set ids", () => {
+    mockResponse({ error: { status: "Invalid BOID" } });
+
+    return client
+      .getInventory("529600")
+      .then(inventory => expect(inventory).toEqual([]));
+  });
+
+  it("should return the inventory", () => {
+    mockResponse({
+      inventory: [
+        { boid: "324854-50", quantity: "4", extra_quantity: "0", alt_link: 0 },
+        { boid: "14440-38", quantity: "1", extra_quantity: "0", alt_link: 0 },
+        { boid: "324854-38", quantity: "2", extra_quantity: "0", alt_link: 0 }
+      ]
+    });
+
+    return client
+      .getInventory("529600")
+      .then(inventory =>
+        expect(inventory).toEqual([
+          { boids: ["324854-50"], quantity: 4 },
+          { boids: ["14440-38"], quantity: 1 },
+          { boids: ["324854-38"], quantity: 2 }
+        ])
+      );
+  });
+
+  it("should group alternate parts", () => {
+    mockResponse({
+      inventory: [
+        { boid: "11111-50", quantity: "4", extra_quantity: "0", alt_link: 1 },
+        { boid: "22222-38", quantity: "1", extra_quantity: "0", alt_link: 0 },
+        { boid: "33333-38", quantity: "4", extra_quantity: "0", alt_link: 1 }
+      ]
+    });
+
+    return client
+      .getInventory("529600")
+      .then(inventory =>
+        expect(inventory).toEqual([
+          { boids: ["11111-50", "33333-38"], quantity: 4 },
+          { boids: ["22222-38"], quantity: 1 }
+        ])
+      );
+  });
+});
+
+describe("getPartNumbers", () => {
+  const defaultResponse = {
+    items: {
+      "543696-53": {
+        boid: "543696-53",
+        type: "Part",
+        ids: [
+          { id: "30136", type: "design_id" },
+          { id: "30136", type: "design_id" },
+          { id: "30136", type: "ldraw" },
+          { id: "30136", type: "peeron_id" },
+          { id: "4114054", type: "item_no" },
+          { id: "4114054", type: "item_no" },
+          { id: "543696-53", type: "boid" }
+        ]
+      },
+      "297160-53": {
+        boid: "297160-53",
+        type: "Part",
+        ids: [
+          { id: "297160-53", type: "boid" },
+          { id: "30132", type: "design_id" },
+          { id: "30132", type: "design_id" },
+          { id: "30132", type: "ldraw" },
+          { id: "30132", type: "peeron_id" },
+          { id: "4105286", type: "item_no" },
+          { id: "4105286", type: "item_no" }
+        ]
+      }
+    }
+  };
+
+  it("should do nothing when empty array passed", () => {
+    mockResponse(defaultResponse);
+    return client.getPartNumbers([]).then(parts => expect(parts).toEqual({}));
+  });
+
+  it("should send one request for less than the chunk size", () => {
+    mockResponse(defaultResponse);
+    return client.getPartNumbers(["543696-53", "297160-53"]).then(parts => {
+      expect(parts).toEqual({
+        "543696-53": "30136",
+        "297160-53": "30132"
+      });
+
+      expect(fetcher.mock.calls.length).toBe(1);
+    });
+  });
+
+  it("should send one request for less than the chunk size", () => {
+    const boids = [...new Array(13).keys()].map(id => id.toString());
+
+    const items = boids.reduce((all, current) => {
+      all[current] = {
+        boid: current,
+        ids: [{ id: "part:" + current, type: "ldraw" }]
+      };
+      return all;
+    }, {});
+
+    mockResponse({ items: items });
+
+    return client.getPartNumbers(boids).then(parts => {
+      expect(Object.keys(parts).length).toBe(boids.length);
+      expect(fetcher.mock.calls.length).toBe(3);
+    });
+  });
+});
