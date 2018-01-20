597701217a1d9cf337a80836ddcb55951ef90967
andy <andydavies2020@gmail.com>
2018-01-20 16:45:00 +0200

    brickowl: export things!

--- projects/BrickOwlApi/src/index.js
+++ projects/BrickOwlApi/src/index.js
@@ -1,22 +1,3 @@
-import Client from "./client";
-import BoidCache from "./boidCache";
-
-const storage = {
-  getMany: boids => Promise.resolve({}),
-  writeMany: boids => Promise.resolve()
-};
-
-const client = new Client(process.env.BRICKOWL_TOKEN);
-const boidCache = new BoidCache(storage, client);
-
-const lookupInventory = setNumber => {
-  const boid = client.boidFromSetNumber(setNumber);
-  const inventory = client.getInventory(boid);
-
-  return boidCache.getMany(inventory.map(part => part.boid)).then(partNumbers =>
-    inventory.map(model => {
-      const { boid, ...part } = model;
-      return Object.assign({}, part, { partNumber: partNumbers[part.boid] });
-    })
-  );
-};
+export { default as BrickOwlApi } from "./api";
+export { default as MemoryStorage } from "./memoryStorage";
+export { default as DynamoStorage } from "./dynamoStorage";
