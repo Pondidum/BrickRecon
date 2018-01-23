2c769f38ef02dff36e1ef247df9ca4a5ed245efd
andy <andydavies2020@gmail.com>
2018-01-23 20:38:37 +0200

    inventory: update local packages to use .package folder

--- projects/InventoryService/build.sh
+++ projects/InventoryService/build.sh
@@ -5,7 +5,7 @@ npm run build
 pushd ./build/app
 
 cp ../../package.json .
-sed -i "s/file:..\//file:..\/..\/..\//g" package.json
+sed -i "s/file:/file:..\/..\//g" package.json
 
 npm install --only prod
 
