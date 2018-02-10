c745895515bbd495c848e908a965bd7bd0183e27
andy <andydavies2020@gmail.com>
2018-02-10 14:32:11 +0200

    inventory: up timeout, as webrequests & set size vary

--- projects/InventoryService/lambda.tf
+++ projects/InventoryService/lambda.tf
@@ -7,7 +7,10 @@ data "archive_file" "inventory_lambda_source" {
 resource "aws_lambda_function" "inventory_service" {
   filename = "${data.archive_file.inventory_lambda_source.output_path}"
   function_name = "${local.name}"
+  timeout = 10
+
   role = "${aws_iam_role.inventory_role.arn}"
+
   handler = "aws.handler"
   runtime = "nodejs6.10"
   source_code_hash = "${base64sha256(file("${data.archive_file.inventory_lambda_source.output_path}"))}"
