4433701781a7554fe134180e3e74b61ffb5aafce
andy <andydavies2020@gmail.com>
2018-02-15 16:52:35 +0200

    bsxProcessor: extract config population

--- projects/BsxProcessor/src/BsxProcessor/Config.cs
+++ projects/BsxProcessor/src/BsxProcessor/Config.cs
@@ -1,7 +1,14 @@
+using System;
+
 namespace BsxProcessor
 {
 	public class Config
 	{
 		public string ImageCacheLambda { get; set; }
+
+		public static Config FromEnvironment() => new Config
+		{
+			ImageCacheLambda = Environment.GetEnvironmentVariable("IMAGECACHE_LAMBDA")
+		};
 	}
 }
--- projects/BsxProcessor/src/BsxProcessor/Handler.cs
+++ projects/BsxProcessor/src/BsxProcessor/Handler.cs
@@ -15,45 +15,43 @@ namespace BsxProcessor
 	public class Handler
 	{
 		private readonly IFileSystem _fileSystem;
+		private readonly ImageCacheDispatcher _imageCacheDispatch;
 
 		public Handler()
 		{
+			var config = Config.FromEnvironment();
+			var lambdaClient = new AmazonLambdaClient();
+
 			_fileSystem = new S3FileSystem(new AmazonS3Client());
+			_imageCacheDispatch = new ImageCacheDispatcher(config, req => lambdaClient.InvokeAsync(req));
 		}
 
 		[LambdaSerializer(typeof(JsonSerializer))]
 		public void Handle(S3Event s3Event)
 		{
-			var config = new Config
-			{
-				ImageCacheLambda = Environment.GetEnvironmentVariable("IMAGECACHE_LAMBDA")
-			};
-
-			HandleRecords(config, s3Event.Records).Wait();
+			HandleRecords(s3Event.Records).Wait();
 		}
 
-		private async Task HandleRecords(Config config, IEnumerable<S3EventNotification.S3EventNotificationRecord> records)
+		private async Task HandleRecords(IEnumerable<S3EventNotification.S3EventNotificationRecord> records)
 		{
-			var lambdaClient = new AmazonLambdaClient();
 			var modelBuilder = new BsxModelBuilder();
-			var imageCacheDispatch = new ImageCacheDispatcher(config, req => lambdaClient.InvokeAsync(req));
 
 			foreach (var record in records)
 			{
 				var document = await _fileSystem.ReadXml(record.S3.Bucket.Name, record.S3.Object.Key);
 				var model = modelBuilder.Build(document);
 
-				imageCacheDispatch.Add(model.Parts);
+				_imageCacheDispatch.Add(model.Parts);
 
 				await _fileSystem.WriteJson(new FileData<BsxModel>
 				{
-					Drive = record.S3.Bucket.Name,
+					Drive = document.Drive,
 					FullPath = $"models/{model.Name}.json",
 					Content = model
 				});
 			}
 
-			await imageCacheDispatch.Dispatch();
+			await _imageCacheDispatch.Dispatch();
 		}
 	}
 }
