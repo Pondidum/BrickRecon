7fdcb75a774fc4f339c84ec5c926330ca7d304ea
andy <andydavies2020@gmail.com>
2018-02-16 22:22:37 +0200

    bsxProcess: extract recordHandler so it can be tested

--- projects/BsxProcessor/src/BsxProcessor/Handler.cs
+++ projects/BsxProcessor/src/BsxProcessor/Handler.cs
@@ -1,57 +1,32 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Amazon.Lambda;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
 using Amazon.Lambda.Serialization.Json;
 using Amazon.S3;
-using Amazon.S3.Util;
-using BsxProcessor.Domain;
 using BsxProcessor.Infrastructure;
-using Environment = System.Environment;
 
 namespace BsxProcessor
 {
 	public class Handler
 	{
-		private readonly IFileSystem _fileSystem;
-		private readonly ImageCacheDispatcher _imageCacheDispatch;
+		private readonly RecordHandler _recordHandler;
 
 		public Handler()
 		{
 			var config = Config.FromEnvironment();
 			var lambdaClient = new AmazonLambdaClient();
 
-			_fileSystem = new S3FileSystem(new AmazonS3Client());
-			_imageCacheDispatch = new ImageCacheDispatcher(config, req => lambdaClient.InvokeAsync(req));
+			var fileSystem = new S3FileSystem(new AmazonS3Client());
+			var imageCacheDispatch = new ImageCacheDispatcher(config, req => lambdaClient.InvokeAsync(req));
+			var modelBuilder = new BsxModelBuilder();
+			
+			_recordHandler = new RecordHandler(fileSystem, imageCacheDispatch, modelBuilder);
 		}
 
 		[LambdaSerializer(typeof(JsonSerializer))]
 		public void Handle(S3Event s3Event)
 		{
-			HandleRecords(s3Event.Records).Wait();
-		}
-
-		private async Task HandleRecords(IEnumerable<S3EventNotification.S3EventNotificationRecord> records)
-		{
-			var modelBuilder = new BsxModelBuilder();
-
-			foreach (var record in records)
-			{
-				var document = await _fileSystem.ReadXml(record.S3.Bucket.Name, record.S3.Object.Key);
-				var model = modelBuilder.Build(document);
-
-				_imageCacheDispatch.Add(model.Parts);
-
-				await _fileSystem.WriteJson(new FileData<BsxModel>
-				{
-					Drive = document.Drive,
-					FullPath = $"models/{model.Name}.json",
-					Content = model
-				});
-			}
-
-			await _imageCacheDispatch.Dispatch();
+			_recordHandler.Execute(s3Event.Records).Wait();
 		}
 	}
 }
---
+++ projects/BsxProcessor/src/BsxProcessor/RecordHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.S3.Util;
+using BsxProcessor.Domain;
+using BsxProcessor.Infrastructure;
+
+namespace BsxProcessor
+{
+	public class RecordHandler
+	{
+		private readonly IFileSystem _fileSystem;
+		private readonly ImageCacheDispatcher _imageCacheDispatch; 
+		private readonly BsxModelBuilder _modelBuilder; 
+
+		public RecordHandler(IFileSystem fileSystem, ImageCacheDispatcher imageCacheDispatch, BsxModelBuilder modelBuilder)
+		{
+			_fileSystem = fileSystem;
+			_imageCacheDispatch = imageCacheDispatch;
+			_modelBuilder = modelBuilder;
+		}
+
+		public async Task Execute(IEnumerable<S3EventNotification.S3EventNotificationRecord> records)
+		{
+			foreach (var record in records)
+			{
+				var document = await _fileSystem.ReadXml(record.S3.Bucket.Name, record.S3.Object.Key);
+				var model = _modelBuilder.Build(document);
+
+				_imageCacheDispatch.Add(model.Parts);
+
+				await _fileSystem.WriteJson(new FileData<BsxModel>
+				{
+					Drive = document.Drive,
+					FullPath = $"models/{model.Name}.json",
+					Content = model
+				});
+			}
+
+			await _imageCacheDispatch.Dispatch();
+		}
+	}
+}
