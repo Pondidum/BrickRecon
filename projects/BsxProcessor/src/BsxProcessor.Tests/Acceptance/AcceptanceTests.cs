75d303fb61514bddc5e16f1cd55ca8212e258bf9
andy <andydavies2020@gmail.com>
2018-03-04 21:45:33 +0200

    bsxProcessor: add acceptance tests

---
+++ projects/BsxProcessor/src/BsxProcessor.Tests/Acceptance/AcceptanceTests.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Amazon.Lambda.Model;
+using Amazon.Lambda.SNSEvents;
+using BsxProcessor.Domain;
+using BsxProcessor.Infrastructure;
+using Newtonsoft.Json;
+using NSubstitute;
+using Shouldly;
+using Xunit;
+
+namespace BsxProcessor.Tests.Acceptance
+{
+	public class AcceptanceTests
+	{
+		private SnsHandler _snsHandler;
+		private InMemoryFileSystem _fileSystem;
+
+		public AcceptanceTests()
+		{
+			var config = new Config
+			{
+				ImageCacheLambda = "wat"
+			};
+
+			var imageRequests = new List<InvokeRequest>();
+
+			var imageCacheDispatch = new ImageCacheDispatcher(config, req =>
+			{
+				imageRequests.Add(req);
+				return Task.FromResult(new InvokeResponse());
+			});
+			var modelBuilder = new BsxModelBuilder();
+
+			_fileSystem = new InMemoryFileSystem();
+			var bsxProcessor = new BsxProcessor(_fileSystem, imageCacheDispatch, modelBuilder);
+
+			_snsHandler = new SnsHandler(bsxProcessor);
+		}
+
+		[Fact]
+		public async Task When_called_from_sns()
+		{
+			var sns = JsonConvert.DeserializeObject<SNSEvent>(await ReadJson());
+
+			await _snsHandler.Handle(sns);
+
+			var written = _fileSystem.Writes.OfType<FileData<BsxModel>>().Single();
+
+			written.ShouldSatisfyAllConditions(
+				() => written.Drive.ShouldBe("brickrecon-dev"),
+				() => written.FullPath.ShouldBe("models/testmodel.json")
+			);
+		}
+
+		private async Task<string> ReadJson()
+		{
+			var assembly = GetType().GetTypeInfo().Assembly;
+
+			using (var ms = assembly.GetManifestResourceStream("BsxProcessor.Tests.Resources.SnsEvent.json"))
+			using (var reader = new StreamReader(ms))
+			{
+				return await reader.ReadToEndAsync();
+			}
+		}
+
+		public class InMemoryFileSystem : IFileSystem
+		{
+			public IEnumerable<object> Writes => _writes;
+
+			private readonly List<object> _writes;
+
+			public InMemoryFileSystem()
+			{
+				_writes = new List<object>();
+			}
+
+			public Task<FileData<XDocument>> ReadXml(string drive, string path)
+			{
+				throw new System.NotImplementedException();
+			}
+
+			public Task WriteJson<TContent>(FileData<TContent> file)
+			{
+				_writes.Add(file);
+				return Task.CompletedTask;
+			}
+		}
+	}
+}
--- projects/BsxProcessor/src/BsxProcessor.Tests/Scratchpad.cs
+++ projects/BsxProcessor/src/BsxProcessor.Tests/Scratchpad.cs
@@ -1,23 +1,26 @@
 using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
+using BsxProcessor.Infrastructure;
+using Newtonsoft.Json;
 using Shouldly;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace BsxProcessor.Tests
 {
 	public class Scratchpad
 	{
+		private readonly ITestOutputHelper _output;
+
+		public Scratchpad(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+		
 		[Fact]
 		public void When_testing_something()
 		{
-			var one = new KeyValuePair<string, int>("2033", 11);
-			var two = new KeyValuePair<string, int>("2033", 11);
-			//var three = new KeyValuePair<string, int>("211", 5);
-			
-			var hash = new HashSet<KeyValuePair<string, int>>();
-			hash.Add(one);
-			
-			hash.ShouldContain(two);
 		}
 	}
 }
