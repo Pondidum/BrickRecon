d6a0b0444f30b6c7a6653c817320755463816287
andy <andy.davies@reaktor.com>
2020-05-22 15:20:03 +0300

    extract view engine

--- command/serve.go
+++ command/serve.go
@@ -1,13 +1,9 @@
 package command
 
 import (
-	"bytes"
-	"fmt"
-	"html/template"
-	"io/ioutil"
+	"mvc/preen"
+
 	"net/http"
-	"path"
-	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -30,10 +26,7 @@ func (c *ServeCommand) Name() string {
 
 func (c *ServeCommand) Run(_ []string) int {
 
-	// pack the templates somehow
-	content, _ := ioutil.ReadFile("./app/index.html")
-
-	layout, err := template.New("layout").Parse(string(content))
+	p, err := preen.NewPreen("./app")
 
 	if err != nil {
 		c.UI.Error(err.Error())
@@ -42,17 +35,6 @@ func (c *ServeCommand) Run(_ []string) int {
 
 	r := mux.NewRouter()
 
-	templates := map[string]*template.Template{}
-	err = addShared(layout, "./app/_shared")
-
-	if err != nil {
-		c.UI.Error(err.Error())
-		return 1
-	}
-
-	addArea(templates, "dashboard")
-	addArea(templates, "create")
-
 	r.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("./app/static/"))))
 
 	r.Handle("/favicon.ico", http.NotFoundHandler())
@@ -60,77 +42,16 @@ func (c *ServeCommand) Run(_ []string) int {
 
 		c.UI.Info(req.URL.String())
 
-		clone, _ := layout.Clone()
-		vars := mux.Vars(req)
+		p.View(w, req, DashboardModel{Models: []string{"one", "two", "three"}})
 
-		area := vars["area"]
-
-		clone.AddParseTree("content", templates[area].Tree)
-
-		var buffer bytes.Buffer
-		err := clone.Execute(&buffer, nil)
-
-		if err != nil {
-			w.WriteHeader(500)
-			w.Write([]byte(err.Error()))
-		}
-
-		w.Write(buffer.Bytes())
 	})
 
 	c.UI.Info("Listening on 127.0.0.1:3000")
 	http.ListenAndServe("127.0.0.1:3000", r)
 
-	if err != nil {
-		c.UI.Error(err.Error())
-		return 1
-	}
-
 	return 0
 }
 
-func addShared(templates *template.Template, pathPrefix string) error {
-
-	files, err := ioutil.ReadDir(pathPrefix)
-
-	if err != nil {
-		return err
-	}
-
-	for _, file := range files {
-
-		name := strings.TrimSuffix(file.Name(), path.Ext(file.Name()))
-		fmt.Printf("Loading %s", name)
-
-		content, err := ioutil.ReadFile(path.Join(pathPrefix, file.Name()))
-
-		if err != nil {
-			return err
-		}
-
-		_, err = templates.New(name).Parse(string(content))
-
-		if err != nil {
-			return err
-		}
-
-		// templates.New()
-		// templates[name] = tpl
-	}
-
-	return nil
-}
-
-func addArea(templates map[string]*template.Template, name string) error {
-
-	content, _ := ioutil.ReadFile("./app/" + name + "/index.html")
-	tpl, err := template.New(name).Parse(string(content))
-
-	if err != nil {
-		return err
-	}
-
-	templates[name] = tpl
-
-	return nil
+type DashboardModel struct {
+	Models []string
 }
---
+++ preen/preen.go
@@ -0,0 +1,156 @@
+package preen
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"path"
+	"strings"
+
+	"github.com/gorilla/mux"
+)
+
+type Preen struct {
+	viewRoot string
+
+	layout    *template.Template
+	templates map[string]*template.Template
+}
+
+func NewPreen(viewRoot string) (Preen, error) {
+	p := Preen{
+		viewRoot:  viewRoot,
+		templates: map[string]*template.Template{},
+	}
+
+	if err := p.loadLayoutRoot(); err != nil {
+		return p, err
+	}
+
+	if err := p.loadShared(); err != nil {
+		return p, err
+	}
+
+	if err := p.loadAreas(); err != nil {
+		return p, err
+	}
+
+	return p, nil
+}
+
+func (p *Preen) loadLayoutRoot() error {
+	content, err := ioutil.ReadFile(path.Join(p.viewRoot, "index.html"))
+
+	if err != nil {
+		return err
+	}
+
+	layout, err := template.New("layout").Parse(string(content))
+
+	if err != nil {
+		return err
+	}
+
+	p.layout = layout
+
+	return nil
+}
+
+func (p *Preen) loadShared() error {
+	shared := path.Join(p.viewRoot, "_shared")
+	files, err := ioutil.ReadDir(shared)
+
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+
+		name := strings.TrimSuffix(file.Name(), path.Ext(file.Name()))
+		fmt.Printf("Loading %s", name)
+
+		content, err := ioutil.ReadFile(path.Join(shared, file.Name()))
+
+		if err != nil {
+			return err
+		}
+
+		_, err = p.layout.New(name).Parse(string(content))
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (p *Preen) loadAreas() error {
+
+	dirs, err := ioutil.ReadDir(p.viewRoot)
+
+	if err != nil {
+		return err
+	}
+
+	for _, dir := range dirs {
+
+		if !dir.IsDir() {
+			continue
+		}
+
+		files, err := ioutil.ReadDir(path.Join(p.viewRoot, dir.Name()))
+
+		if err != nil {
+			return err
+		}
+
+		for _, file := range files {
+
+			if path.Ext(file.Name()) == ".html" {
+
+				templateName := strings.TrimSuffix(file.Name(), path.Ext(file.Name()))
+
+				if templateName == "index" {
+					templateName = dir.Name()
+				} else {
+					templateName = dir.Name() + "/" + "templateName"
+				}
+
+				content, _ := ioutil.ReadFile(path.Join(p.viewRoot, dir.Name(), file.Name()))
+
+				tpl, err := p.layout.New(templateName).Parse(string(content))
+
+				if err != nil {
+					return err
+				}
+
+				p.templates[templateName] = tpl
+			}
+		}
+	}
+
+	return nil
+
+}
+
+func (p *Preen) View(w http.ResponseWriter, req *http.Request, model interface{}) {
+
+	vars := mux.Vars(req)
+	area := vars["area"]
+
+	clone, _ := p.layout.Clone()
+	clone.AddParseTree("content", p.templates[area].Tree)
+
+	var buffer bytes.Buffer
+	err := clone.Execute(&buffer, model)
+
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+	}
+
+	w.Write(buffer.Bytes())
+}
