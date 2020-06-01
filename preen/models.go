db17e5e4b9db5f26dd2fe2f713ffd08eb0690b4b
andy <andy.davies@reaktor.com>
2020-06-01 18:19:07 +0300

    preen: define an error model

--- preen/models.go
+++ preen/models.go
@@ -3,3 +3,11 @@ package preen
 type Redirect struct {
 	URL string
 }
+
+type Error struct {
+	ErrorMessage string
+}
+
+func ErrorModel(err error) Error {
+	return Error{ErrorMessage: err.Error()}
+}
