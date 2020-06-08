e1377dba2fe56e389a5b9977bd2e501728825460
andy <andy.davies@reaktor.com>
2020-06-08 21:55:52 +0300

    move distributor files around

---
+++ distributor/distributor.go
@@ -0,0 +1,67 @@
+package distributor
+
+import "reflect"
+
+type MessageHandlerFunc func(Message)
+
+type Distributor struct {
+	topics map[string][]MessageHandlerFunc
+}
+
+func NewDistributor() *Distributor {
+	return &Distributor{
+		topics: map[string][]MessageHandlerFunc{},
+	}
+}
+
+func (d *Distributor) RegisterFor(messageType Message, handler MessageHandlerFunc) {
+
+	name := messageName(messageType)
+
+	if _, found := d.topics[name]; !found {
+		d.topics[name] = []MessageHandlerFunc{}
+	}
+
+	d.topics[name] = append(d.topics[name], handler)
+}
+
+func (d *Distributor) Dispatch(message Message) {
+
+	name := messageName(message)
+	listeners, found := d.topics[name]
+
+	if !found {
+		return
+	}
+
+	for _, handler := range listeners {
+		go handler(message)
+	}
+
+	return
+}
+
+func (d *Distributor) DispatchSync(message Message) {
+
+	name := messageName(message)
+	listeners, found := d.topics[name]
+
+	if !found {
+		return
+	}
+
+	for _, handler := range listeners {
+		handler(message)
+	}
+
+	return
+}
+
+func messageName(event interface{}) string {
+	t := reflect.TypeOf(event)
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	return t.Name()
+}
--- distributor/message.go
+++ distributor/message.go
@@ -1,7 +1,5 @@
 package distributor
 
-import "reflect"
-
 type MessageMeta struct {
 }
 
@@ -9,70 +7,4 @@ func (m *MessageMeta) meta() *MessageMeta { return m }
 
 type Message interface {
 	meta() *MessageMeta
-}
-
-// -------------
-
-type MessageHandlerFunc func(Message)
-
-type Distributor struct {
-	topics map[string][]MessageHandlerFunc
-}
-
-func NewDistributor() *Distributor {
-	return &Distributor{
-		topics: map[string][]MessageHandlerFunc{},
-	}
-}
-
-func (d *Distributor) RegisterFor(messageType Message, handler MessageHandlerFunc) {
-
-	name := messageName(messageType)
-
-	if _, found := d.topics[name]; !found {
-		d.topics[name] = []MessageHandlerFunc{}
-	}
-
-	d.topics[name] = append(d.topics[name], handler)
-}
-
-func (d *Distributor) Dispatch(message Message) {
-
-	name := messageName(message)
-	listeners, found := d.topics[name]
-
-	if !found {
-		return
-	}
-
-	for _, handler := range listeners {
-		go handler(message)
-	}
-
-	return
-}
-
-func (d *Distributor) DispatchSync(message Message) {
-
-	name := messageName(message)
-	listeners, found := d.topics[name]
-
-	if !found {
-		return
-	}
-
-	for _, handler := range listeners {
-		handler(message)
-	}
-
-	return
-}
-
-func messageName(event interface{}) string {
-	t := reflect.TypeOf(event)
-	if t.Kind() == reflect.Ptr {
-		t = t.Elem()
-	}
-
-	return t.Name()
 }
