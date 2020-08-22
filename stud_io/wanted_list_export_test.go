178ea42d53839d16e51e5ef75894276fc29f093a
andy <andy.davies@reaktor.com>
2020-08-22 14:05:01 +0300

    stud.io: implement wantedlist export

---
+++ stud_io/wanted_list_export_test.go
@@ -0,0 +1,42 @@
+package stud_io
+
+import (
+	"brickrecon/lego"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSerializationFormat(t *testing.T) {
+
+	parts := wantedList{
+		Items: []wantedListItem{
+			{Type: "P", ID: "2540", Color: lego.BrickLinkColour(85), Quantity: 9},
+			{Type: "P", ID: "11477", Color: lego.BrickLinkColour(59), Quantity: 2},
+		},
+	}
+
+	expected := strings.TrimSpace(`
+<INVENTORY>
+  <ITEM>
+    <ITEMTYPE>P</ITEMTYPE>
+    <ITEMID>2540</ITEMID>
+    <COLOR>85</COLOR>
+    <MINQTY>9</MINQTY>
+  </ITEM>
+  <ITEM>
+    <ITEMTYPE>P</ITEMTYPE>
+    <ITEMID>11477</ITEMID>
+    <COLOR>59</COLOR>
+    <MINQTY>2</MINQTY>
+  </ITEM>
+</INVENTORY>
+`)
+
+	xml, err := marshal(parts)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, xml)
+
+}
