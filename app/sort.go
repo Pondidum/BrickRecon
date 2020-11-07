2f3213280502fed7f66b941fd49ea9b0a017181d
andy <andy.davies@reaktor.com>
2020-11-07 23:05:28 +0200

    app: basic part sorting expansion

--- app/sort.go
+++ app/sort.go
@@ -7,11 +7,14 @@ import (
 
 type sorter struct {
 	kit string
+
+	sortKey string
 }
 
 func createSorter(pc *preen.PreenContext) *sorter {
 	return &sorter{
-		kit: pc.QueryValue("kit"),
+		kit:     pc.QueryValue("kit"),
+		sortKey: pc.QueryValue("sort"),
 	}
 }
 
@@ -21,7 +24,16 @@ func (s *sorter) Parts(parts []*PartWithKitPart) []*PartWithKitPart {
 		return s.byKitAddition(parts)
 	}
 
-	return s.byPartThenColour(parts)
+	if s.sortKey == "name" {
+		return s.byName(parts)
+	}
+
+	if s.sortKey == "colour" {
+		return s.byColour(parts)
+	}
+
+	return s.byName(parts)
+
 }
 
 func (s *sorter) clone(parts []*PartWithKitPart) []*PartWithKitPart {
@@ -30,6 +42,41 @@ func (s *sorter) clone(parts []*PartWithKitPart) []*PartWithKitPart {
 	for i, p := range parts {
 		result[i] = p
 	}
+
+	return result
+}
+
+func (s *sorter) byName(parts []*PartWithKitPart) []*PartWithKitPart {
+	result := s.clone(parts)
+
+	sort.Slice(result, func(x, y int) bool {
+		l := result[x]
+		r := result[y]
+
+		if l.Name == r.Name {
+			return l.ColourID < r.ColourID
+		}
+
+		return l.Name < r.Name
+	})
+
+	return result
+}
+
+func (s *sorter) byColour(parts []*PartWithKitPart) []*PartWithKitPart {
+	result := s.clone(parts)
+
+	sort.Slice(result, func(x, y int) bool {
+		l := result[x]
+		r := result[y]
+
+		if l.ColourID == r.ColourID {
+			return l.Name < r.Name
+		}
+
+		return l.ColourID < r.ColourID
+
+	})
 
 	return result
 }
@@ -52,24 +99,6 @@ func (s *sorter) byKitAddition(parts []*PartWithKitPart) []*PartWithKitPart {
 		}
 
 		return l.KitQuantity > 0
-	})
-
-	return result
-}
-
-func (s *sorter) byPartThenColour(parts []*PartWithKitPart) []*PartWithKitPart {
-
-	result := s.clone(parts)
-
-	sort.Slice(result, func(x int, y int) bool {
-		l := result[x]
-		r := result[y]
-
-		if l.ID == r.ID {
-			return l.ColourID < r.ColourID
-		}
-
-		return l.ID < r.ID
 	})
 
 	return result
