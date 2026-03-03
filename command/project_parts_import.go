d3174b7b17d1ba0e8c6b1d2685917057dea73e72
andy <andy@sammalmaa.fi>
2026-03-03 22:38:03 +0200

    project.parts.import: move query to storage to avoid transaction

--- command/project_parts_import.go
+++ command/project_parts_import.go
@@ -8,7 +8,6 @@ import (
 	"brickrecon/storage"
 	"brickrecon/tracing"
 	"context"
-	"database/sql"
 	"fmt"
 	"os"
 
@@ -71,14 +70,8 @@ func (c *ProjectPartsImportCommand) Execute(ctx context.Context, config *config.
 	}
 	defer content.Close()
 
-	tx, err := store.BeginTx(ctx)
-	if err != nil {
-		return tracing.Error(span, err)
-	}
-	defer tx.Rollback()
-
 	getPart := func(partId lego.PartId) (*lego.Part, error) {
-		parts, err := GetPart(ctx, tx, partId)
+		parts, err := storage.FindMatchingParts(ctx, store, partId)
 		if err != nil {
 			return nil, err
 		}
@@ -110,91 +103,23 @@ func (c *ProjectPartsImportCommand) Execute(ctx context.Context, config *config.
 		return nil
 	}
 
+	fmt.Printf("Adding parts...")
 	if err := project.AddParts(parts); err != nil {
 		return tracing.Error(span, err)
 	}
+	fmt.Println("Done")
 
+	fmt.Print("Adding Stock...")
 	if err := project.AddStock(stock); err != nil {
 		return tracing.Error(span, err)
 	}
+	fmt.Println("Done")
 
 	if err := store.SaveAggregate(ctx, project); err != nil {
-		return tracing.Error(span, err)
-	}
-
-	if err := tx.Commit(); err != nil {
 		return tracing.Error(span, err)
 	}
 
 	fmt.Println("Done")
 
 	return nil
-}
-
-func GetPart(ctx context.Context, tx *sql.Tx, partId lego.PartId) ([]*lego.Part, error) {
-	stmt := `
-	
-		select rp.part_num, rp.name, rpc.name, 0 "priority"
-		from rebrickable_parts rp 
-		join rebrickable_part_categories rpc on rpc.id = rp.category_id 
-		where rp.part_num = @part_num 
-
-		union
-
-		select rp.part_num, rp.name, rpc.name, 1 "priority"
-		from rebrickable_parts rp 
-		join rebrickable_part_categories rpc on rpc.id = rp.category_id 
-		join ldraw_alternate_ids lai on rp.part_num = lai.part_num
-		where lai.alt_part_num = @part_num 
-
-		union
-	
-		select rp.part_num, rp.name, rp.name, 2 "priority"
-		from rebrickable_parts rp 
-		join (
-			select lai.alt_part_num "part_num"
-			from ldraw_alternate_ids lai 
-			where lai.part_num in (
-				select lai.part_num
-				from ldraw_alternate_ids lai 
-				where lai.alt_part_num = @part_num
-			)) alt on alt.part_num = rp.part_num
-
-		union
-
-		select rp.part_num, rp.name, rpc.name, 3 "priority"
-		from rebrickable_parts rp 
-		join rebrickable_part_categories rpc on rpc.id = rp.category_id 
-		join ldraw_moved_parts lmp on lmp.new_part_num  = rp.part_num
-		where lmp.old_part_num = @part_num 
-
-		union
-
-		select rp.part_num , rp.name, rpc.name, 4 "priority"
-		from rebrickable_parts rp
-		join rebrickable_part_categories rpc on rpc.id = rp.category_id
-		where rp.part_num like concat(@part_num, '%')
-
-		order by priority, rp.part_num
- 	`
-
-	rows, err := tx.QueryContext(ctx, stmt, sql.Named("part_num", partId))
-	if err != nil {
-		return nil, err
-	}
-
-	parts := []*lego.Part{}
-	for rows.Next() {
-
-		part := &lego.Part{}
-		priority := 0
-		if err := rows.Scan(&part.Id, &part.Name, &part.Category, &priority); err != nil {
-			return nil, err
-		}
-
-		parts = append(parts, part)
-	}
-
-	return parts, nil
-
 }
